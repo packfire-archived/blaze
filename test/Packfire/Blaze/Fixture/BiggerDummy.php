06b6c884c8360742798201c58af521044233eec1
Sam-Mauris Yong <mauris@hotmail.sg>
2013-06-16 22:28:33 +0800

    bigger dummy implementation

---
+++ test/Packfire/Blaze/Fixture/BiggerDummy.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Packfire\Blaze\Fixture;
+
+/**
+ * @entity dummies
+ */
+class BiggerDummy extends Dummy
+{
+    /**
+     * @attribute Age
+     * @var integer
+     */ 
+    public $age;
+}
