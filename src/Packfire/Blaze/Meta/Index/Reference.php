7e49399eb272816bd40fe2aa310db6dc1e20fa49
Sam-Mauris Yong <mauris@hotmail.sg>
2013-06-23 19:14:55 +0800

    reference class implementation

---
+++ src/Packfire/Blaze/Meta/Index/Reference.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Packfire\Blaze\Meta\Index;
+
+use Packfire\Blaze\Meta\IndexInterface;
+use Packfire\Blaze\Meta\AttributeCollection;
+use Packfire\Blaze\Utility\IndexUtility;
+
+class Reference implements IndexInterface
+{
+    protected $name;
+
+    protected $attributes;
+
+    public function __construct($name, AttributeCollection $attributes)
+    {
+        $this->name = $name;
+        $this->attributes = $attributes;
+    }
+
+    public function name()
+    {
+        return $this->name;
+    }
+
+    public function attributes()
+    {
+        return $this->attributes;
+    }
+}
