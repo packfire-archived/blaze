cb852a3b4a39706cb14eca3dc739edae98f8ba4d
Sam-Mauris Yong <mauris@hotmail.sg>
2013-06-23 19:02:33 +0800

    primarykeytest implemented

---
+++ test/Packfire/Blaze/Meta/Index/PrimaryKeyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Packfire\Blaze\Meta\Index;
+
+class PrimaryKeyTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstruct()
+    {
+        $index = new PrimaryKey();
+        $this->assertInstanceOf('Packfire\\Blaze\\Meta\\AttributeCollection', $index->attributes());
+        $this->assertEquals('pk_', $index->name());
+    }
+}
