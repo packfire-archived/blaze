7a8b22a06abbfdfd4f8fc47094fca32cc4521e96
Sam-Mauris Yong <mauris@hotmail.sg>
2013-06-23 17:30:22 +0800

    implemented AttributeUtility and test

---
+++ src/Packfire/Blaze/Utility/AttributeUtility.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Packfire\Blaze\Utility;
+
+use Packfire\Blaze\Meta\AttributeCollection;
+
+class AttributeUtility
+{
+    public static function listing(AttributeCollection $collection, $separator = ', ', $wrapper = '`')
+    {
+        $compileList = array();
+        foreach ($collection as $attribute) {
+            $compileList[] = $wrapper . $attribute->name() . $wrapper;
+        }
+        return implode($compileList, $separator);
+    }
+}
---
+++ test/Packfire/Blaze/Utility/AttributeUtilityTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Packfire\Blaze\Utility;
+
+use Packfire\Blaze\Meta\AttributeCollection;
+use Packfire\Blaze\Meta\Attribute;
+
+class AttributeUtilityTest extends \PHPUnit_Framework_TestCase
+{
+    public function testListing()
+    {
+        $collection = new AttributeCollection();
+        $collection->add(new Attribute('test1', 'test1' , 'TEXT'));
+        $collection->add(new Attribute('test2', 'test2' , 'int'));
+
+        $result = AttributeUtility::listing($collection);
+        $this->assertEquals('`test1`, `test2`', $result);;
+    }
+    
+    public function testListingCustom()
+    {
+        $collection = new AttributeCollection();
+        $collection->add(new Attribute('test1', 'test1' , 'TEXT'));
+        $collection->add(new Attribute('test2', 'test2' , 'int'));
+
+        $result = AttributeUtility::listing($collection, ' | ', '"');
+        $this->assertEquals('"test1" | "test2"', $result);;
+    }
+}
