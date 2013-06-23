3ffecf8a6e75cf9f8063606852b86ce5ac7aa09e
Sam-Mauris Yong <mauris@hotmail.sg>
2013-06-23 17:48:55 +0800

    psr-2 fixing

--- test/Packfire/Blaze/Utility/AttributeUtilityTest.php
+++ test/Packfire/Blaze/Utility/AttributeUtilityTest.php
@@ -10,20 +10,20 @@ class AttributeUtilityTest extends \PHPUnit_Framework_TestCase
     public function testListing()
     {
         $collection = new AttributeCollection();
-        $collection->add(new Attribute('test1', 'test1' , 'TEXT'));
-        $collection->add(new Attribute('test2', 'test2' , 'int'));
+        $collection->add(new Attribute('test1', 'test1', 'TEXT'));
+        $collection->add(new Attribute('test2', 'test2', 'int'));
 
         $result = AttributeUtility::listing($collection);
-        $this->assertEquals('`test1`, `test2`', $result);;
+        $this->assertEquals('`test1`, `test2`', $result);
     }
-    
+
     public function testListingCustom()
     {
         $collection = new AttributeCollection();
-        $collection->add(new Attribute('test1', 'test1' , 'TEXT'));
-        $collection->add(new Attribute('test2', 'test2' , 'int'));
+        $collection->add(new Attribute('test1', 'test1', 'TEXT'));
+        $collection->add(new Attribute('test2', 'test2', 'int'));
 
         $result = AttributeUtility::listing($collection, ' | ', '"');
-        $this->assertEquals('"test1" | "test2"', $result);;
+        $this->assertEquals('"test1" | "test2"', $result);
     }
 }
--- test/Packfire/Blaze/Utility/IndexUtilityTest.php
+++ test/Packfire/Blaze/Utility/IndexUtilityTest.php
@@ -10,8 +10,8 @@ class IndexUtilityTest extends \PHPUnit_Framework_TestCase
     public function testName()
     {
         $index = new PrimaryKey();
-        $index->attributes()->add(new Attribute('test1', 'test1' , 'TEXT'));
-        $index->attributes()->add(new Attribute('test2', 'test2' , 'int'));
+        $index->attributes()->add(new Attribute('test1', 'test1', 'TEXT'));
+        $index->attributes()->add(new Attribute('test2', 'test2', 'int'));
 
         $result = IndexUtility::name($index);
         $this->assertEquals('idx_test1_test2', $result);
@@ -20,8 +20,8 @@ class IndexUtilityTest extends \PHPUnit_Framework_TestCase
     public function testNameCustom()
     {
         $index = new PrimaryKey();
-        $index->attributes()->add(new Attribute('test1', 't@st1' , 'TEXT'));
-        $index->attributes()->add(new Attribute('test2', 't@st2' , 'int'));
+        $index->attributes()->add(new Attribute('test1', 't@st1', 'TEXT'));
+        $index->attributes()->add(new Attribute('test2', 't@st2', 'int'));
 
         $result = IndexUtility::name($index, 'FK_');
         $this->assertEquals('FK_t_st1_t_st2', $result);
