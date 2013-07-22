4529b2598c4cfed8e29e7fc0fe332391ab66582c
Sam-Mauris Yong <mauris@hotmail.sg>
2013-07-22 21:10:48 +0800

    updated Attribute indexes

--- src/Packfire/Blaze/Meta/Attribute/Attribute.php
+++ src/Packfire/Blaze/Meta/Attribute/Attribute.php
@@ -2,6 +2,8 @@
 
 namespace Packfire\Blaze\Meta\Attribute;
 
+use Packfire\Blaze\Meta\Index\IndexCollection;
+
 class Attribute implements AttributeInterface
 {
     protected $property;
@@ -10,11 +12,14 @@ class Attribute implements AttributeInterface
 
     protected $type;
 
+    protected $indexes;
+
     public function __construct($property, $name, $type)
     {
         $this->property = $property;
         $this->name = $name;
         $this->type = $type;
+        $this->indexes = new IndexCollection();
     }
 
     public function property()
@@ -30,5 +35,10 @@ class Attribute implements AttributeInterface
     public function type()
     {
         return $this->type;
+    }
+
+    public function indexes()
+    {
+        return $this->indexes;
     }
 }
