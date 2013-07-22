16f27c09ad9c53856e693e0ad33c5f66c9812722
Sam-Mauris Yong <mauris@hotmail.sg>
2013-07-22 21:23:59 +0800

    fixed up attributeloadertest

--- test/Packfire/Blaze/Meta/Attribute/AttributeLoaderTest.php
+++ test/Packfire/Blaze/Meta/Attribute/AttributeLoaderTest.php
@@ -7,8 +7,8 @@ class AttributeLoaderTest extends \PHPUnit_Framework_TestCase
     public function testLoad()
     {
         $property = new \ReflectionProperty('Packfire\\Blaze\\Fixture\\Dummy', 'name');
-        $attribute = Attribute::load($property);
-        $this->assertInstanceOf('Packfire\\Blaze\\Meta\\Attribute', $attribute);
+        $attribute = AttributeLoader::load($property);
+        $this->assertInstanceOf('Packfire\\Blaze\\Meta\\Attribute\\Attribute', $attribute);
         $this->assertEquals('name', $attribute->property());
         $this->assertEquals('Name', $attribute->name());
         $this->assertEquals('string', $attribute->type());
@@ -17,8 +17,8 @@ class AttributeLoaderTest extends \PHPUnit_Framework_TestCase
     public function testLoadTypeAnnotation()
     {
         $property = new \ReflectionProperty('Packfire\\Blaze\\Fixture\\BiggerDummy', 'age');
-        $attribute = Attribute::load($property);
-        $this->assertInstanceOf('Packfire\\Blaze\\Meta\\Attribute', $attribute);
+        $attribute = AttributeLoader::load($property);
+        $this->assertInstanceOf('Packfire\\Blaze\\Meta\\Attribute\\Attribute', $attribute);
         $this->assertEquals('age', $attribute->property());
         $this->assertEquals('Age', $attribute->name());
         $this->assertEquals('TINYINT', $attribute->type());
@@ -27,18 +27,26 @@ class AttributeLoaderTest extends \PHPUnit_Framework_TestCase
     public function testLoadEmpty()
     {
         $property = new \ReflectionProperty('Packfire\\Blaze\\Fixture\\BiggerDummy', 'height');
-        $attribute = Attribute::load($property);
-        $this->assertInstanceOf('Packfire\\Blaze\\Meta\\Attribute', $attribute);
+        $attribute = AttributeLoader::load($property);
+        $this->assertInstanceOf('Packfire\\Blaze\\Meta\\Attribute\\Attribute', $attribute);
         $this->assertEquals('height', $attribute->property());
         $this->assertEquals('height', $attribute->name());
         $this->assertEquals('double', $attribute->type());
+    }
+
+    public function testLoadIndexes()
+    {
+        $property = new \ReflectionProperty('Packfire\\Blaze\\Fixture\\BiggerDummy', 'dummyId');
+        $attribute = AttributeLoader::load($property);
+        $this->assertInstanceOf('Packfire\\Blaze\\Meta\\Attribute\\Attribute', $attribute);
+        $this->assertCount(1, $attribute->indexes());
     }
 
     public function testFail()
     {
         // non-existing attribute tag
         $property = new \ReflectionProperty('Packfire\\Blaze\\Meta\\Entity\\Entity', 'name');
-        $attribute = Attribute::load($property);
+        $attribute = AttributeLoader::load($property);
         $this->assertNull($attribute);
     }
 }
